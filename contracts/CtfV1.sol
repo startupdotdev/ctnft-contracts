197bf74c02b25c0774756ccc051c9d97dbf6b3a2
John Mosesman <johnmosesman@users.noreply.github.com>
2022-04-17 20:55:42 -0500

    Add commit reveal functions (#2)
    * Commit answer
    
    * Test cleanup
    
    * Rename variable for clarity
    
    * Use proper secret in tests
    
    * Use bytes32 to store the hash as that’s the proper type that comes back from keccak, not string
    
    * Checkpoint
    
    * Finish reveal
    
    * Cleanup tests and add todos for events

--- contracts/CtfV1.sol
+++ contracts/CtfV1.sol
@@ -4,22 +4,23 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-// Note: this is very insecure. :)
 contract CtfV1 {
   using Counters for Counters.Counter;
 
   struct Ctf {
     string name;
-    string secret;
+    bytes32 secret;
     address creator;
     uint256 balance;
+    bool isActive;
   }
 
   Counters.Counter public _ctfIds;
   mapping(uint256 => Ctf) public ctfs;
+  mapping(uint256 => mapping(address => bytes32)) public answers;
 
-  function createCtf(string memory name, string memory secret) public payable {
-    require(msg.value > 0, "An Ether prize is required to create a CTF");
+  function createCtf(string memory name, bytes32 secret) public payable {
+    require(msg.value > 0, "Ether required to create a CTF");
 
     _ctfIds.increment();
     uint256 newId = _ctfIds.current();
@@ -28,11 +29,62 @@ contract CtfV1 {
       name: name,
       secret: secret,
       creator: msg.sender,
-      balance: msg.value
+      balance: msg.value,
+      isActive: true
     });
+
+    // TODO: emit event
   }
 
-  function solveCtf(uint256 ctfId, string memory secret) public {
-    //
+  // TODO: Revert if the contest is over
+  function commitAnswer(uint256 ctfId, bytes32 saltedHash) public {
+    Ctf memory ctf = ctfs[ctfId];
+
+    require(ctf.isActive == true, "CTF not active or doesn't exist");
+
+    require(
+      answers[ctfId][msg.sender] == bytes32(0),
+      "Already submitted an answer"
+    );
+
+    answers[ctfId][msg.sender] = saltedHash;
+
+    // TODO: emit event
+  }
+
+  // TODO: Add re-entrancy guard for fun and profit?
+  function revealAnswer(
+    uint256 ctfId,
+    string memory answer,
+    string memory salt
+  ) public {
+    require(
+      answers[ctfId][msg.sender] != bytes32(0),
+      "No previous answer committed"
+    );
+
+    require(
+      keccak256(abi.encodePacked(answer, salt)) == answers[ctfId][msg.sender],
+      "Doesn't match submitted answer"
+    );
+
+    Ctf storage ctf = ctfs[ctfId];
+
+    require(ctf.isActive == true, "CTF not active or doesn't exist");
+
+    require(
+      keccak256(abi.encodePacked(answer)) == ctf.secret,
+      "Submission doesn't match secret"
+    );
+
+    // You won!
+    uint256 prize = ctf.balance;
+    ctf.balance = 0;
+    ctf.isActive = false;
+
+    (bool sent, bytes memory data) = msg.sender.call{ value: prize }("");
+    require(sent, "Failed to send Ether");
+
+    // TODO: emit Event here
   }
 }
--- test/ctf-v1-test.js
+++ test/ctf-v1-test.js
@@ -2,65 +2,148 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("CtfV1", function () {
+  let contract;
+  let secret = "secret-withsalt";
+  let secretHash = ethers.utils.id(secret);
+  let ctfId;
+  let etherValue = ethers.utils.parseEther("1.0");
+
+  beforeEach(async () => {
+    [signer1] = await ethers.getSigners();
+
+    const CtfV1 = await ethers.getContractFactory("CtfV1");
+    contract = await CtfV1.deploy();
+    await contract.deployed();
+  });
+
+  const createCtf = async () => {
+    let txn = await contract.createCtf("Party Town", secretHash, {
+      value: etherValue,
+    });
+    await txn.wait();
+
+    ctfId = 1;
+  };
+
   describe("createCtf()", () => {
     it("reverts without ether sent", async () => {
-      [signer1, signer2] = await ethers.getSigners();
-
-      const CtfV1 = await ethers.getContractFactory("CtfV1");
-      const contract = await CtfV1.deploy();
-      await contract.deployed();
-
-      // With no value reverts
       await expect(
-        contract.createCtf("Party Town", "partypass")
-      ).to.be.revertedWith("An Ether prize is required to create a CTF");
+        contract.createCtf("Party Town", secretHash)
+      ).to.be.revertedWith("Ether required to create a CTF");
     });
 
     it("with ether sent creates a new CTF", async function () {
-      [signer1, signer2] = await ethers.getSigners();
+      await createCtf();
 
-      const CtfV1 = await ethers.getContractFactory("CtfV1");
-      const contract = await CtfV1.deploy();
-      await contract.deployed();
+      let result = await contract.ctfs(ctfId);
 
-      let etherValue = ethers.utils.parseEther("1.0");
-      txn = await contract
-        .connect(signer2)
-        .createCtf("Party Town 2", "partypass2", { value: etherValue });
-      await txn.wait();
-
-      result = await contract.ctfs(1);
-
-      expect(result.name).to.equal("Party Town 2");
-      expect(result.creator).to.equal(signer2.address);
+      expect(result.name).to.equal("Party Town");
+      expect(result.creator).to.equal(signer1.address);
       expect(result.balance).to.equal(etherValue);
+      expect(result.isActive).to.equal(true);
+      expect(result.secret).to.equal(secretHash);
     });
   });
 
-  // describe("solveCtf()", () => {
-  //   let contract;
-  //   const ctfId = 1;
+  describe("commitAnswer()", async () => {
+    it("reverts if the ctf doesn't exist", async () => {
+      await expect(
+        contract.commitAnswer(1, ethers.utils.id("boop + salt"))
+      ).to.be.revertedWith("CTF not active or doesn't exist");
+    });
 
-  //   beforeEach(async () => {
-  //     [signer1, signer2] = await ethers.getSigners();
+    it("accepts an answer", async () => {
+      await createCtf();
 
-  //     const CtfV1 = await ethers.getContractFactory("CtfV1");
-  //     contract = await CtfV1.deploy();
-  //     await contract.deployed();
+      let answer = ethers.utils.id("boop + salt");
+      let commitTxn = await contract.commitAnswer(ctfId, answer);
+      await commitTxn.wait();
 
-  //     let txn = await contract.createCtf("Party Town", "correct-answer");
-  //     await txn.wait();
-  //   });
+      let commitAnswer = await contract.answers(ctfId, signer1.address);
+      expect(commitAnswer).to.equal(answer);
+    });
 
-  //   it("with the correct answer stops the contest and pays the balance", async () => {
-  //     await expect(
-  //       await contract.solveCtf(ctfId, "correct-answer")
-  //     ).to.changeEtherBalance(signer1, ethers.utils.parseEther("1"));
+    it("returns empty if the user hasn't submitted an answer yet", async () => {
+      await createCtf();
 
-  //     let ctf = await contract.ctfs(1);
-  //     expect(ctf.isActive).to.equal(false);
-  //   });
+      let commitAnswer = await contract.answers(ctfId, signer1.address);
+      expect(commitAnswer).to.equal(ethers.constants.HashZero);
+    });
 
-  //   it("with the incorrect answer stops the contest and pays the balance", async () => {});
-  // });
+    it("reverts if the user has already submitted an answer yet", async () => {
+      await createCtf();
+
+      let answer = ethers.utils.id("boop + salt");
+      let commitTxn = await contract.commitAnswer(ctfId, answer);
+      await commitTxn.wait();
+
+      await expect(contract.commitAnswer(ctfId, answer)).to.be.revertedWith(
+        "Already submitted an answer"
+      );
+    });
+  });
+
+  describe("revealAnswer()", async () => {
+    it("reverts if there isn't a committed answer", async () => {
+      await createCtf();
+
+      await expect(
+        contract.revealAnswer(ctfId, "answer", "the salt")
+      ).to.be.revertedWith("No previous answer committed");
+    });
+
+    it("reverts if the submitted answer doesn't match the committed answer", async () => {
+      await createCtf();
+
+      let answer = "boop";
+      let salt = "-thesalt";
+      let answerHash = ethers.utils.id(`${answer}${salt}`);
+
+      let commitTxn = await contract.commitAnswer(ctfId, answerHash);
+      await commitTxn.wait();
+
+      await expect(
+        contract.revealAnswer(ctfId, "not", "the same")
+      ).to.be.revertedWith("Doesn't match submitted answer");
+    });
+
+    // TODO: after we can stop contests add this test
+    // it("reverts if the ctf doesn't exist", async () => {});
+
+    it("reverts if the submitted answer doesn't match the secret", async () => {
+      await createCtf();
+
+      let answer = "boop";
+      let salt = "-thesalt";
+      let answerHash = ethers.utils.id(`${answer}${salt}`);
+
+      let commitTxn = await contract.commitAnswer(ctfId, answerHash);
+      await commitTxn.wait();
+
+      await expect(
+        contract.revealAnswer(ctfId, answer, salt)
+      ).to.be.revertedWith("Submission doesn't match secret");
+    });
+
+    it("succeeds and pays the winner if the answer matches the secret", async () => {
+      await createCtf();
+
+      let answer = secret;
+      let salt = "-moresalt";
+      let answerHash = ethers.utils.id(`${answer}${salt}`);
+
+      let commitTxn = await contract.commitAnswer(ctfId, answerHash);
+      await commitTxn.wait();
+
+      await expect(
+        await contract.revealAnswer(ctfId, answer, salt)
+      ).to.changeEtherBalances(
+        [contract, signer1],
+        [ethers.utils.parseEther("-1.0"), ethers.utils.parseEther("1.0")]
+      );
+
+      let ctf = await contract.ctfs(1);
+      expect(ctf.isActive).to.equal(false);
+    });
+  });
 });
